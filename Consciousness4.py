83036e47809751554a156bc998324b8a7d381edc
mamaofthree579-ship-it <mamaofthree579@gmail.com>
2026-03-01 10:38:26 -0500

    Update Consciousness4.py

--- Consciousness4.py
+++ Consciousness4.py
@@ -1,12 +1,17 @@
 import streamlit as st
 import numpy as np
 
-st.title("EEG chart")
+st.title("EEG chart with column picker")
 uploaded=st.file_uploader("Upload")
 if uploaded:
     data=np.genfromtxt(uploaded, delimiter=None)
     st.write("shape:", data.shape)
-    eeg=data[:,0] if data.ndim>1 else data
+    if data.ndim>1:
+        col=st.sidebar.slider("Column",0,data.shape[1]-1,0)
+        eeg=data[:,col]
+    else:
+        eeg=data
+    st.write("std:", np.std(eeg))
     st.line_chart(eeg)
 else:
     st.write("Upload file")
