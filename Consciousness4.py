dc3a8f713b4b1f112cb25a93b36fb70f9eded47b
mamaofthree579-ship-it <mamaofthree579@gmail.com>
2026-03-04 15:52:11 -0500

    Update Consciousness4.py

--- Consciousness4.py
+++ Consciousness4.py
@@ -1,21 +1,25 @@
 import streamlit as st
 import numpy as np
 
-st.title("EEG raw plot")
+st.title("EEG safe plot")
+if 'eeg' not in st.session_state:
+    st.session_state.eeg=None
+
 uploaded=st.file_uploader("Upload")
-if uploaded:
+if uploaded and st.session_state.eeg is None:
     try:
         data=np.genfromtxt(uploaded, delimiter=None)
-        st.write("shape:", data.shape)
         if data.ndim>1:
             col=st.sidebar.slider("Column",0,data.shape[1]-1,0)
             eeg=data[:,col]
         else:
             eeg=data
-        eeg = np.nan_to_num(eeg)
-        st.write("std:", np.std(eeg))
-        st.line_chart(eeg)
+        st.session_state.eeg=np.nan_to_num(eeg)
     except Exception as ex:
-        st.error("Error: "+str(ex))
+        st.error(str(ex))
+
+eeg=st.session_state.eeg
+if eeg is not None:
+    st.line_chart(eeg)
 else:
     st.write("Upload file")
