bf92734146c40fc2dc95eb517e1b930c4bcb6e3e
mamaofthree579-ship-it <mamaofthree579@gmail.com>
2026-03-09 14:37:22 -0400

    Update Bilby.py

--- Bilby.py
+++ Bilby.py
@@ -5,18 +5,18 @@ import bilby, os, tempfile
 
 os.environ["BILBY_INCLUDE_GLOBAL_METADATA"] = "False"
 
-st.title("CGUP Ladder + Bilby (warning‑free)")
+st.title("CGUP Ladder + Bilby (final)")
 
-# sliders
+# sidebar sliders
 alpha = st.sidebar.slider("α*", 0.0, 0.2, 0.08, 0.005)
-lam   = st.sidebar.slider("λ",   0.3, 0.7, 0.5, 0.01)
+lam   = st.sidebar.slider("λ", 0.3, 0.7, 0.5, 0.01)
 beta = 0.4
 omega_GR = 0.3737
 
-def cgup_freq(alpha, lam, n):
-    return omega_GR + (alpha**2)*omega_GR*(lam**n + beta*lam**(n+1))
+def cgup_freq(a, l, n):
+    return omega_GR + (a**2)*omega_GR*(l**n + beta*l**(n+1))
 
-# plot ladder
+# ladder plot
 freqs = [cgup_freq(alpha, lam, n) for n in range(5)]
 fig, ax = plt.subplots()
 for f in freqs:
@@ -26,26 +26,27 @@ ax.axvspan(omega_GR-0.06, omega_GR+0.06, color='gray', alpha=0.2,
 ax.set_xlabel("Frequency"); ax.set_yticks([]); ax.legend()
 st.pyplot(fig)
 
-# Bilby toy run
+# custom Bilby likelihood
+class CGUPLike(bilby.likelihood.Likelihood):
+    def __init__(self, data):
+        super().__init__(parameters={'alpha': None, 'lam': None})
+        self.data = data
+    def log_likelihood(self):
+        model = cgup_freq(self.parameters['alpha'],
+                          self.parameters['lam'], 0)
+        return -0.5*((self.data - model)/0.01)**2
+
 if st.button("Run Bilby toy fit"):
     st.write("Running Bilby…")
     data_freq = omega_GR + np.random.normal(0, 0.01)
-
-    def loglike(p):
-        model = cgup_freq(p['alpha'], p['lam'], 0)
-        return -0.5*((data_freq - model)/0.01)**2
-
+    like = CGUPLike(data_freq)
     priors = bilby.core.prior.PriorDict({
         'alpha': bilby.core.prior.Uniform(0, 0.2),
         'lam':   bilby.core.prior.Uniform(0.3, 0.7)
     })
-
-    likelihood = bilby.likelihood.Likelihood()
-    likelihood.log_likelihood = loglike   # <-- set here, not in run_sampler
-
     with tempfile.TemporaryDirectory() as d:
         res = bilby.run_sampler(
-            likelihood=likelihood,
+            likelihood=like,
             priors=priors,
             sampler='dynesty',
             nlive=200,
@@ -56,4 +57,4 @@ if st.button("Run Bilby toy fit"):
         st.pyplot(res.plot_corner())
     st.success("Done!")
 
-st.caption("Swap the toy loglike for a GravitationalWaveTransient likelihood to use real data.")
+st.caption("Replace CGUPLike with a GWTransient likelihood to run on real GW250114 data.")
