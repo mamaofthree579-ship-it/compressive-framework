6fd80f910099ee3e6031c238c2265257e717b258
mamaofthree579-ship-it <mamaofthree579@gmail.com>
2026-03-09 19:36:09 -0400

    Update Wave.py

--- Wave.py
+++ Wave.py
@@ -4,4 +4,4 @@ h = np.sin(np.linspace(0, 2*np.pi, 100))
 np.save("h_placeholder.npy", h.real)
 # load it back to verify
 loaded = np.load("h_placeholder.npy")
-print(loaded[:5])
+st.write(loaded[:5])
