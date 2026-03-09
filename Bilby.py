c8c0eb8c4813308b439baee93f96b0e56bf778be
mamaofthree579-ship-it <mamaofthree579@gmail.com>
2026-03-09 14:40:41 -0400

    Update Bilby.py

--- Bilby.py
+++ Bilby.py
@@ -1,60 +1,62 @@
 import streamlit as st
-import numpy as np
-import matplotlib.pyplot as plt
 import bilby, os, tempfile
+from bilby.gw import detector, waveform, likelihood
 
 os.environ["BILBY_INCLUDE_GLOBAL_METADATA"] = "False"
 
-st.title("CGUP Ladder + Bilby (final)")
+st.title("CGUP GW250114 Analysis")
 
-# sidebar sliders
+# sliders for CGUP params
 alpha = st.sidebar.slider("α*", 0.0, 0.2, 0.08, 0.005)
-lam   = st.sidebar.slider("λ", 0.3, 0.7, 0.5, 0.01)
+lam = st.sidebar.slider("λ", 0.3, 0.7, 0.5, 0.01)
 beta = 0.4
 omega_GR = 0.3737
 
-def cgup_freq(a, l, n):
-    return omega_GR + (a**2)*omega_GR*(l**n + beta*l**(n+1))
+# 1. download real data
+if st.button("Load GW250114 data"):
+    st.write("Fetching H1/L1 strain from GWOSC…")
+    data = detector.get_event_data('GW250114', interferometers=['H1','L1'],
+                                   duration=4, sampling_frequency=2048)
+    st.success("Data loaded")
 
-# ladder plot
-freqs = [cgup_freq(alpha, lam, n) for n in range(5)]
-fig, ax = plt.subplots()
-for f in freqs:
-    ax.axvline(f, color='tab:blue', lw=2)
-ax.axvspan(omega_GR-0.06, omega_GR+0.06, color='gray', alpha=0.2,
-           label='GR 90% bound')
-ax.set_xlabel("Frequency"); ax.set_yticks([]); ax.legend()
-st.pyplot(fig)
+    # 2. CGUP waveform wrapper
+    class CGUPWaveform(waveform.WaveformGenerator):
+        def __init__(self, alpha, lam):
+            super().__init__(
+                duration=4, sampling_frequency=2048,
+                waveform_function=waveform.get_td_waveform,
+                waveform_arguments={'waveform_approximant':'pSEOBNRv5PHM'}
+            )
+            self.alpha, self.lam = alpha, lam
+        def frequency_domain_strain(self, params):
+            h = super().frequency_domain_strain(params)
+            # toy shift: scale each mode by CGUP factor
+            for i in range(len(h)):
+                h[i] *= 1 + (self.alpha**2)*(self.lam**i)
+            return h
 
-# custom Bilby likelihood
-class CGUPLike(bilby.likelihood.Likelihood):
-    def __init__(self, data):
-        super().__init__(parameters={'alpha': None, 'lam': None})
-        self.data = data
-    def log_likelihood(self):
-        model = cgup_freq(self.parameters['alpha'],
-                          self.parameters['lam'], 0)
-        return -0.5*((self.data - model)/0.01)**2
+    wf = CGUPWaveform(alpha, lam)
+    like = likelihood.GravitationalWaveTransient(interferometers=data,
+                                                  waveform_generator=wf)
 
-if st.button("Run Bilby toy fit"):
-    st.write("Running Bilby…")
-    data_freq = omega_GR + np.random.normal(0, 0.01)
-    like = CGUPLike(data_freq)
     priors = bilby.core.prior.PriorDict({
-        'alpha': bilby.core.prior.Uniform(0, 0.2),
-        'lam':   bilby.core.prior.Uniform(0.3, 0.7)
+        'mass_1': bilby.core.prior.Uniform(20, 80),
+        'mass_2': bilby.core.prior.Uniform(20, 80),
+        'alpha': bilby.core.prior.DeltaFunction(alpha),
+        'lam': bilby.core.prior.DeltaFunction(lam),
+        #... other needed params fixed or given priors...
     })
+
+    st.write("Running Bilby on GW250114 (this will take minutes)…")
     with tempfile.TemporaryDirectory() as d:
         res = bilby.run_sampler(
             likelihood=like,
             priors=priors,
             sampler='dynesty',
-            nlive=200,
+            nlive=400,
             outdir=d,
-            label='cgup_demo',
+            label='cgup_gw250114',
             verbose=False
         )
-        st.pyplot(res.plot_corner())
+        st.pyplot(res.plot_corner(['mass_1','mass_2']))
     st.success("Done!")
-
-st.caption("Replace CGUPLike with a GWTransient likelihood to run on real GW250114 data.")
