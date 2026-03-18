bac325b881c88da3d734981bb13646beb490841f
mamaofthree579-ship-it <mamaofthree579@gmail.com>
2026-03-18 08:16:58 -0400

    Create Boat.py

---
+++ Boat.py
@@ -0,0 +1,24 @@
+QH1 = {
+    "length": 4.5,
+    "width": 1.2,
+    "hull_spacing": 0.6,
+    "draft": 0.25,
+
+    "bow_shape": "tapered_split",
+    "hull_type": "catamaran",
+
+    "surface_texture": {
+        "type": "ribbed",
+        "direction": "longitudinal",
+        "amplitude": 0.002,
+        "frequency": 120
+    },
+
+    "tail_system": {
+        "fin_count": 5,
+        "lengths": [1.2, 1.0, 0.8, 1.0, 1.2],
+        "angles_deg": [-20, -10, 0, 10, 20],
+        "flexibility": 0.3,
+        "curvature": 0.5
+    }
+}
