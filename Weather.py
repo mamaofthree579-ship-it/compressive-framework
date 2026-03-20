4a48b86b486b54049bbf1bb8d1abcf3bed9c55e7
mamaofthree579-ship-it <mamaofthree579@gmail.com>
2026-03-19 22:29:25 -0400

    Update Weather.py

--- Weather.py
+++ Weather.py
@@ -1,34 +1,34 @@
 import numpy as np
 import matplotlib.pyplot as plt
-import matplotlib.animation as animation
 
-# (All the parameters and the update function are the same as before)
+# --- Simulation Parameters for a single frame ---
+# This section defines all the necessary variables.
+# The error happens if this part is missing.
+GRID_SIZE = 100
+FRAME = 50 
 
-# --- Setup the Grid ---
+# --- Setup and Calculation ---
+# The line below is where the error occurred because GRID_SIZE wasn't defined yet.
 x = np.arange(0, GRID_SIZE)
 y = np.arange(0, GRID_SIZE)
 X, Y = np.meshgrid(x, y)
 
-# --- Animation Function ---
+# Calculate the waves at this single moment in time
+storm_wave = 1.0 * np.sin(0.5 * X - 1.0 * FRAME)
+dist_from_chant = np.sqrt((X - 70)**2 + (Y - 50)**2)
+chant_wave = 1.2 * np.sin(0.5 * dist_from_chant - 1.0 * FRAME + np.pi)
+chant_wave *= np.exp(-(dist_from_chant / (GRID_SIZE / 5))**2)
+combined_field = storm_wave + chant_wave
+
+# --- Plot the single image ---
 fig, ax = plt.subplots()
-def update(frame):
-    ax.clear()
-    storm_wave = 1.0 * np.sin(0.5 * X - 1.0 * frame)
-    dist_from_chant = np.sqrt((X - 70)**2 + (Y - 50)**2)
-    chant_wave = 1.2 * np.sin(0.5 * dist_from_chant - 1.0 * frame + np.pi)
-    chant_wave *= np.exp(-(dist_from_chant / (GRID_SIZE / 5))**2)
-    combined_field = storm_wave + chant_wave
-    im = ax.imshow(combined_field, cmap='viridis', vmin=-2, vmax=2)
-    ax.set_title(f"Wave Interference Simulation (Time: {frame})")
-    ax.set_xticks([])
-    ax.set_yticks([])
-    return [im]
+im = ax.imshow(combined_field, cmap='viridis', vmin=-2, vmax=2)
+ax.set_title(f"Wave Interference (Single Frame)")
+ax.set_xticks([])
+ax.set_yticks([])
 
-# --- Run the Animation ---
-ani = animation.FuncAnimation(fig, update, frames=100, blit=True)
-
-# Use this line to show an interactive pop-up window
+# This line will try to open an interactive window to show the plot
 plt.show()
 
-# Use this line if you want to save to a file instead
-# ani.save('wave_simulation.gif', writer='imagemagick', fps=15)
+# You can uncomment the line below to save the image as a file instead
+# plt.savefig('static_wave_frame.png')
